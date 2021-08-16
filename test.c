5c31b775942e6f045284b6a735ef943b1b3026c4
luffy981 <jhonsmith981@hotmail.com>
2021-08-16 03:53:22 -0500

    Function str_tok fixed

--- test.c
+++ test.c
@@ -2,36 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-char * str_tok(char * s, const char * toks)
+char * str_tok(char *buffer, const char *delimiter)
 {
-	static char * last = '\0';
-	char * ret = '\0';
-	char * p;
-	if (toks)
+	static char *save = NULL;
+	char *string = NULL;
+	char *set = NULL;
+
+	buffer = buffer ? buffer : save;
+	if (buffer)
 	{
-		s = s ? s : last;
-		if (s)
+		set = buffer;
+		if (!*set)
 		{
-			p = s;
-			if (!*p)
+			save = '\0';
+		} else
+		{
+			string = set;
+			while (*set != *delimiter && *set != '\0')
 			{
-				last = '\0';
-			} else
-			{
-				ret = p;
-				while (*p != *toks && *p != '\0')
-				{
-					p++;
-				}
-				*p = '\0';
-				p++;
-				last =p;
+				set++;
 			}
+			*set = '\0';
+			set++;
+			save = set;
 		}
 	}
-	return ret;
+	return (string);
 }
-char **tokenizer(char *buffer , char *delimiter)
+char **tokenizer(char *buffer, char *delimiter)
 {
 	char **tokens = NULL;
 	size_t i = 0;
