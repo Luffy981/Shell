fe7e91a4d12875ec5d51e2e4d06355a69ae7507f
luffy981 <jhonsmith981@hotmail.com>
2021-08-20 04:20:33 -0500

    File headers

--- file.h
+++ file.h
@@ -4,21 +4,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <limits.h>
+
 typedef struct variables
 {
-	char *buffer;
-	char **arrays;
+	char *buffer; /*Buffer to getline*/
+	char **arrays; /*Array of pointers to strings*/
 }vars_t;
 
 typedef struct arguments
 {
-	char *n;
-	void (*f)(vars_t *);
+	char *n; /*String to do match*/
+	char (*f)(vars_t *r); /*Pointer to function after do match*/
 }arg_t;
 
+int main (int argc , char *argv[] , char **enviroment);
 char **tokenizer(char *buffer , char *delimiter);
-int main (int argc , char **argv[] , char **enviroment);
-void  (*match (vars_t *vars))(vars_t *vars);
-void file_exit(vars_t *vars);
-void file_number(vars_t  *vars);
+char (*match (vars_t *m))(vars_t *n);
+char file_exit(vars_t *vars);
+char file_number(vars_t  *vars);
+char list_files(vars_t *env);
+char current_directory(vars_t *pwd);
+char delete_file_or_directory(vars_t *rm);
+char clean_trash(vars_t *clean);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *str_cat(char *s1, char *s2);
+int _strlen(char *s);
+void check_path(vars_t *vars, char **enviroment);
+int check2_path(vars_t *vars, char **enviroment);
+void free_struct(vars_t *vars);
 #endif
